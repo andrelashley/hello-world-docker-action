931becbf8b5280f2a69d93186301ed056450895d
André Lashley <andre.lashley@gmail.com>
2019-12-06 15:29:43 -0800

    Use oc login command for service account

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ echo "Hello $1"
 time=$(date)
 echo ::set-output name=time::$time
 
-OUTPUT="$(oc version)"
+OUTPUT="$(oc login https://console.pathfinder.gov.bc.ca:8443 --token=$SERVICE_ACCOUNT_TOKEN)"
 echo "${OUTPUT}"
