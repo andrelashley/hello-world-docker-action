0a338374bf52475c37868742486c2f8f5a5e1d8f
André Lashley <andre.lashley@gmail.com>
2019-12-06 15:32:18 -0800

    Switch to the correct project

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,6 @@ echo ::set-output name=time::$time
 
 OUTPUT="$(oc login https://console.pathfinder.gov.bc.ca:8443 --token=$SERVICE_ACCOUNT_TOKEN)"
 echo "${OUTPUT}"
+
+OUTPUT="$(oc project ntu9uh-tools)"
+echo "${OUTPUT}"
